5c9aa477c8ec756e516ef9adcc73de9727fd0e3c
Daniel Munoz <danni-0516@hotmail.com>
2025-03-06 12:06:16 -0600

    Los pacientes que no fallecieron se cambia los dias transcurridos hasta la fecha de ultima actualización

---
+++ regresiones/regresion2024.R
@@ -0,0 +1,21 @@
+# Cargar el paquete
+library(survival)
+library(survminer)
+
+
+# Convertir comorbilidades a factores (opcional pero recomendado)
+comorbilidades <- c('DIABETES', 'HIPERTENSION','CARDIOVASCULAR', 
+                    'OBESIDAD', 'TABAQUISMO')
+
+# Convierte las comorbilidades a factores
+df_cox2024[comorbilidades] <- lapply(df_cox2024[comorbilidades], factor)
+
+# Definir la fórmula del modelo de Cox
+formula_cox <- as.formula("Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES + HIPERTENSION + CARDIOVASCULAR + OBESIDAD + TABAQUISMO")
+
+# Ajustar el modelo de Cox
+modelo_cox <- coxph(formula_cox, data = df_cox2024)
+
+# Ver el resumen del modelo
+summary(modelo_cox)
+
