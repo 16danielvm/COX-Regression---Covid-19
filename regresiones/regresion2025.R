d46d9132447c8df2e3c06630fc70318b119e9268
Daniel Munoz <danni-0516@hotmail.com>
2025-03-10 18:07:47 -0600

    Implementación terminada

--- regresiones/regresion2025.R
+++ regresiones/regresion2025.R
@@ -3,14 +3,6 @@ library(survival)
 library(survminer)
 
 
-# Convertir comorbilidades a factores (opcional pero recomendado)
-#comorbilidades <- c('NEUMONIA', 'DIABETES', 'EPOC', 'ASMA', 'INMUSUPR',
-#                    'HIPERTENSION', 'CARDIOVASCULAR', 
-#                    'OBESIDAD', 'RENAL_CRONICA', 'TABAQUISMO')
-
-# Convierte las comorbilidades a factores
-#df_cox2025[comorbilidades] <- lapply(df_cox2025[comorbilidades], factor)
-
 # Definir la fórmula del modelo de Cox
 formula_cox <- as.formula("Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA + DIABETES + EPOC + 
                           ASMA + INMUSUPR + HIPERTENSION + OBESIDAD + 
@@ -23,34 +15,93 @@ modelo_cox <- coxph(formula_cox, data = df_cox2025)
 # Ver el resumen del modelo
 summary(modelo_cox)
 
-
 ### Test de Schoenfeld
-test_schoenfeld <- cox.zph(modelo_cox)
-print(test_schoenfeld)
+test_proporcionalidad <- cox.zph(modelo_cox)
+print(test_proporcionalidad)
+plot(test_proporcionalidad)
 
-# Gráfico de residuos de Schoenfeld
-ggcoxzph(cox.zph(modelo_cox))
-
-# Crear la interacción NEUMONIA * log(tiempo)
-df_cox2025$NEUMONIA_TIEMPO <- df_cox2025$NEUMONIA * log(df_cox2025$DIAS_SINTOMAS_DEF + 1)
-
-# Nueva fórmula con estratificación
-formula_cox_corr <- as.formula("Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA + NEUMONIA_TIEMPO +
-                               DIABETES + EPOC + ASMA + INMUSUPR + HIPERTENSION + 
-                               OBESIDAD + RENAL_CRONICA + TABAQUISMO + strata(CARDIOVASCULAR)")
-
-# Ajustar el modelo con estratificación
-modelo_cox_corr <- coxph(formula_cox_corr, data = df_cox2025)
-
-# Ver el resumen del modelo corregido
-summary(modelo_cox_corr)
-
-# Curva de supervivencia para NEUMONIA
-ggsurvplot(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025))
-             
-test_schoenfeld <- cox.zph(modelo_cox)
-print(test_schoenfeld)
-
-### Multicolinealidad
+# SUPUESTO DE COLINEALIDAD:
 library(car)
 vif(modelo_cox)
+
+### CORRECCION MODELO
+# Corrección del modelo usando tt() para las variables que no cumplieron el supuesto
+modelo_cox_corr <- coxph(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ 
+                           DIABETES + EPOC + ASMA + INMUSUPR + 
+                           HIPERTENSION + OBESIDAD + RENAL_CRONICA +
+                           TABAQUISMO + tt(NEUMONIA) + tt(CARDIOVASCULAR), 
+                         data = df_cox2025,
+                         tt = function(x, t, ...) x * log1p(t))
+
+# Resumen del modelo corregido
+summary(modelo_cox_corr)
+
+
+modelo_sin_tt <- coxph(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ 
+                         DIABETES + EPOC + ASMA + INMUSUPR + 
+                         HIPERTENSION + OBESIDAD + RENAL_CRONICA +
+                         TABAQUISMO, 
+                       data = df_cox2025)
+
+# Evaluación del supuesto de riesgos proporcionales para las variables restantes
+cox.zph(modelo_sin_tt)
+
+library(survival)
+library(survminer)
+
+# Curva de supervivencia para NEUMONIA
+surv_neumonia <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
+ggsurvplot(surv_neumonia, 
+           data = df_cox2025,
+           risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - NEUMONIA",
+           legend.title = "Neumonía",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+
+# Curva de supervivencia para CARDIOVASCULAR
+surv_cardio <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ CARDIOVASCULAR, data = df_cox2025)
+ggsurvplot(surv_cardio, 
+           data = df_cox2025,
+           risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - ENF. CARDIOVASCULAR",
+           legend.title = "Enf. Cardiovascular",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+
+# Curvas de supervivencia para las variables que SI cumplieron el supuesto
+surv_diabetes <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025)
+ggsurvplot(surv_diabetes, 
+           data = df_cox2025,
+           risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - DIABETES",
+           legend.title = "Diabetes",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+summary(surv_diabetes, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))  # Evaluar hasta 3 años
+
+
+# Curvas de supervivencia para las variables que SI cumplieron el supuesto
+surv_neumonia <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
+ggsurvplot(surv_neumonia, 
+           data = df_cox2025,
+           risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - NEUMONIA",
+           legend.title = "Diabetes",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+summary(surv_neumonia, times = c(30, 60, 90, 180, 365))
