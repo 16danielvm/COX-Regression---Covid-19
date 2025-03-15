a03f52b3ab99d7b4fcb0048b015507e166b488dd
Daniel Munoz <danni-0516@hotmail.com>
2025-03-14 19:08:03 -0600

    Implementación regresion cox 2024 - 2025

--- regresiones/regresion2025.R
+++ regresiones/regresion2025.R
@@ -3,11 +3,12 @@ library(survival)
 library(survminer)
 
 
+hist(df_cox2025$DIAS_SINTOMAS_DEF)
+
 # Definir la fórmula del modelo de Cox
 formula_cox <- as.formula("Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA + DIABETES + EPOC + 
                           ASMA + INMUSUPR + HIPERTENSION + OBESIDAD + 
                           CARDIOVASCULAR + RENAL_CRONICA + TABAQUISMO")
-
 
 # Ajustar el modelo de Cox
 modelo_cox <- coxph(formula_cox, data = df_cox2025)
@@ -46,40 +47,37 @@ modelo_sin_tt <- coxph(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~
 # Evaluación del supuesto de riesgos proporcionales para las variables restantes
 cox.zph(modelo_sin_tt)
 
+test_proporcionalidad_sin_tt <- cox.zph(modelo_cox_sin_tt)
+print(test_proporcionalidad_sin_tt)
+
+
 library(survival)
 library(survminer)
 
-# Curva de supervivencia para NEUMONIA
-surv_neumonia <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
-ggsurvplot(surv_neumonia, 
+# Curvas de supervivencia para las variables que SI cumplieron el supuesto
+surv_NEUMONIA <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
+ggsurvplot(surv_NEUMONIA, 
            data = df_cox2025,
-           risk.table = TRUE, 
+           #risk.table = TRUE, 
            pval = TRUE, 
            conf.int = TRUE,
            title = "Curva de Supervivencia - NEUMONIA",
-           legend.title = "Neumonía",
+           legend.title = "Diabetes",
            legend.labs = c("No", "Sí"),
            xlab = "Tiempo (días)",
            ylab = "Probabilidad de Supervivencia")
+summary(surv_NEUMONIA, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))
 
-# Curva de supervivencia para CARDIOVASCULAR
-surv_cardio <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ CARDIOVASCULAR, data = df_cox2025)
-ggsurvplot(surv_cardio, 
-           data = df_cox2025,
-           risk.table = TRUE, 
-           pval = TRUE, 
-           conf.int = TRUE,
-           title = "Curva de Supervivencia - ENF. CARDIOVASCULAR",
-           legend.title = "Enf. Cardiovascular",
-           legend.labs = c("No", "Sí"),
-           xlab = "Tiempo (días)",
-           ylab = "Probabilidad de Supervivencia")
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
 
-# Curvas de supervivencia para las variables que SI cumplieron el supuesto
-surv_diabetes <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025)
-ggsurvplot(surv_diabetes, 
+
+
+surv_DIABETES <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025)
+ggsurvplot(surv_DIABETES, 
            data = df_cox2025,
-           risk.table = TRUE, 
+           #risk.table = TRUE, 
            pval = TRUE, 
            conf.int = TRUE,
            title = "Curva de Supervivencia - DIABETES",
@@ -87,21 +85,73 @@ ggsurvplot(surv_diabetes,
            legend.labs = c("No", "Sí"),
            xlab = "Tiempo (días)",
            ylab = "Probabilidad de Supervivencia")
-summary(surv_diabetes, times = c(30, 60, 90, 180, 365))
+summary(surv_DIABETES, times = c(30, 60, 90, 180, 365))
 summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025),
-        times = c(30, 60, 90, 180, 365, 400))  # Evaluar hasta 3 años
+        times = c(30, 60, 90, 180, 365, 400))
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ DIABETES, data = df_cox2025)
 
 
-# Curvas de supervivencia para las variables que SI cumplieron el supuesto
-surv_neumonia <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ NEUMONIA, data = df_cox2025)
-ggsurvplot(surv_neumonia, 
+surv_EPOC <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ EPOC, data = df_cox2025)
+ggsurvplot(surv_EPOC, 
            data = df_cox2025,
-           risk.table = TRUE, 
+           #risk.table = TRUE, 
            pval = TRUE, 
            conf.int = TRUE,
-           title = "Curva de Supervivencia - NEUMONIA",
+           title = "Curva de Supervivencia - EPOC",
            legend.title = "Diabetes",
            legend.labs = c("No", "Sí"),
            xlab = "Tiempo (días)",
            ylab = "Probabilidad de Supervivencia")
-summary(surv_neumonia, times = c(30, 60, 90, 180, 365))
+summary(surv_EPOC, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ EPOC, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ EPOC, data = df_cox2025)
+
+surv_HIPERTENSION <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ HIPERTENSION, data = df_cox2025)
+ggsurvplot(surv_HIPERTENSION, 
+           data = df_cox2025,
+           #risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - HIPERTENSION",
+           legend.title = "Diabetes",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+summary(surv_HIPERTENSION, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ HIPERTENSION, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ HIPERTENSION, data = df_cox2025)
+
+surv_RENAL_CRONICA <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ RENAL_CRONICA, data = df_cox2025)
+ggsurvplot(surv_RENAL_CRONICA, 
+           data = df_cox2025,
+           #risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - RENAL_CRONICA",
+           legend.title = "Diabetes",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+summary(surv_RENAL_CRONICA, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ RENAL_CRONICA, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ RENAL_CRONICA, data = df_cox2025)
+
+
+surv_TABAQUISMO <- survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ TABAQUISMO, data = df_cox2025)
+ggsurvplot(surv_TABAQUISMO, 
+           data = df_cox2025,
+           #risk.table = TRUE, 
+           pval = TRUE, 
+           conf.int = TRUE,
+           title = "Curva de Supervivencia - TABAQUISMO",
+           legend.title = "Diabetes",
+           legend.labs = c("No", "Sí"),
+           xlab = "Tiempo (días)",
+           ylab = "Probabilidad de Supervivencia")
+summary(surv_TABAQUISMO, times = c(30, 60, 90, 180, 365))
+summary(survfit(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ TABAQUISMO, data = df_cox2025),
+        times = c(30, 60, 90, 180, 365, 400))
+survdiff(Surv(DIAS_SINTOMAS_DEF, DEFUNCION) ~ TABAQUISMO, data = df_cox2025)
